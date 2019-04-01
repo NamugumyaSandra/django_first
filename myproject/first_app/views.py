c40a63d156255a7419c508a6d04ca98468a94f21
Namugumya Sandra <salyn134@gmail.com>
2019-04-01 15:55:05 +0300

    Create a starter project and application.

---
+++ myproject/first_app/views.py
@@ -0,0 +1,11 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+# Create your views here.
+class HomePageView(TemplateView):
+    def get(self,request,**kwargs):
+        return render(request,'index.html',context=None)
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
